301ea11951b8a13c132626bee9be42281258443c
Lakshan Perera <lakshanperera@lakshan-pereras-macbook.local>
2010-10-04 12:19:37 +0530

    parse the JSON error messages

--- lib/curdbee/base.rb
+++ lib/curdbee/base.rb
@@ -62,7 +62,7 @@ module CurdBee
       when 403
         raise(CurdBee::Error::Forbidden.new(response), "Your action was forbidden.")
       when 422
-        raise(CurdBee::Error::BadRequest.new(response), response.body)
+        raise(CurdBee::Error::BadRequest.new(response), JSON.parse(response.body).join(" "))
       when 404
         raise(CurdBee::Error::NotFound.new(response), "Resource not found.")
       when 500
