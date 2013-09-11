f2b769f3f961312b5cd4367594e779832b4cdd66
Lahiru Jayaratne <lahiruj5@gmail.com>
2013-09-11 17:47:44 +0530

    Update parser.rb
    Removed require 'crack' and Crack::JSON.parse(body) replaced only with json

--- lib/curdbee/parser.rb
+++ lib/curdbee/parser.rb
@@ -1,11 +1,11 @@
-require 'crack'
+require 'json'
 
 module CurdBee
   class Parser < HTTParty::Parser
 
     def parse
       begin
-        Crack::JSON.parse(body)
+        JSON.parse(body)
       rescue => e
         raise(CurdBee::Error::UnexpectedResponse.new(e.message), body)
       end
